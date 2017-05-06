c21860a53a42e9d38f6367709c703909d67df7f4
smael123 <ismael.alma@gmail.com>
2017-05-05 23:31:31 -0500

    implemented the map cannot test rn cause of twitter limit

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -2,6 +2,9 @@ import os
 from configparser import ConfigParser
 import tweepy
 from textblob import TextBlob
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from matplotlib.patches import Polygon
 
 class Tweet:
 	def __init__(self, text, mood = -1.0):
@@ -49,16 +52,36 @@ class TwitterQuery:
 				self.neutral_tweets.append(tweet_obj)
 	
 	def get_perc_negative_tweets(self):
-		return (len(self.negative_tweets) / len(self.tweet_list)) * 100
+		try:
+			return (len(self.negative_tweets) / len(self.tweet_list)) * 100
+		except ZeroDivisionError:
+			return 0
 	
 	def get_perc_neutral_tweets(self):
-		return (len(self.neutral_tweets) / len(self.tweet_list)) * 100
+		try:
+			return (len(self.neutral_tweets) / len(self.tweet_list)) * 100
+		except ZeroDivisionError:
+			return 0
 	
 	def get_perc_positive_tweets(self):
-		return (len(self.positive_tweets) / len(self.tweet_list)) * 100
+		try:
+			return (len(self.positive_tweets) / len(self.tweet_list)) * 100
+		except ZeroDivisionError:
+			return 0
 	
 	def get_num_tweets(self):
 		return len(self.tweet_list)
+	
+	def get_max_sentiment(self):
+		sentiment_dict = {}
+		
+		sentiment_dict[len(self.negative_tweets)] = 'negative'
+		sentiment_dict[len(self.neutral_tweets)] = 'neutral'
+		sentiment_dict[len(self.positive_tweets)] = 'positive'
+		
+		key_list = sentiment_dict.keys()
+		
+		return sentiment_dict[max(key_list)]
 	
 	def get_positive_tweets(self):
 		str = ''
@@ -139,7 +162,7 @@ class TwitterQuery:
 
 cfg = ConfigParser()
 
-cfg.read(os.path.join(os.pardir, os.pardir, 'twitter.ini'))
+cfg.read(os.path.join('necessary_files', 'twitter.ini'))
 twitter_consumer_key = cfg.get('twitter_keys', 'consumer_key')
 twitter_consumer_secret = cfg.get('twitter_keys', 'consumer_secret')
 twitter_access_token = cfg.get('twitter_keys', 'access_token')
@@ -153,12 +176,10 @@ api = tweepy.API(auth)
 places = api.geo_search(query='USA', granularity='country')
 usa_id = places[0].id
 
-str_query = input("What string would you like to search for?")
+str_query = input("What string would you like to search for? ")
 
 usa_test = TwitterQuery(api, str_query, usa_id, "USA")
 print(usa_test)
-
-#Let's crate a map
 
 state_list = ("Alabama", "Alaska", "Arizona", "Arkansas", "California","Colorado","Connecticut","Delaware",
 "Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine",
@@ -170,10 +191,46 @@ state_list = ("Alabama", "Alaska", "Arizona", "Arkansas", "California","Colorado
 state_dict = {}
 state_queries = []
 
-for state in state_list[0:2]:
+for state in state_list:
 	places = api.geo_search(query=state, granularity='admin')
 	state_dict[state] = places[0].id
 	query = TwitterQuery(api, str_query, state_dict[state], state)
 	#query.get_tweets()
 	state_queries.append(query)
 	print(query)
+
+#Let's create a map	
+map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
+        projection='lcc',lat_1=33,lat_2=45,lon_0=-95)
+
+# load the shapefile, use the name 'states'
+map.readshapefile('st99_d00', name='states', drawbounds=True)
+
+# collect the state names from the shapefile attributes so we can
+# look up the shape obect for a state by it's name
+state_names = []
+for shape_dict in map.states_info:
+    state_names.append(shape_dict['NAME'])
+
+ax = plt.gca() # get current axes instance
+
+# get Texas and draw the filled polygon
+seg = map.states[state_names.index('Texas')]
+poly = Polygon(seg, facecolor='red',edgecolor='red')
+ax.add_patch(poly)
+
+for state_query in state_queries:
+	if (state_query.get_max_sentiment == 'positive'):
+		seg = map.states[state_names.index(state_query.region_str)]
+		poly = Polygon(seg, facecolor='red',edgecolor='red')
+		ax.add_patch(poly)
+	elif (state_query.get_max_sentiment == 'neutral'):
+		seg = map.states[state_names.index(state_query.region_str)]
+		poly = Polygon(seg, facecolor='gray',edgecolor='gray')
+		ax.add_patch(poly)
+	else:
+		seg = map.states[state_names.index(state_query.region_str)]
+		poly = Polygon(seg, facecolor='blue',edgecolor='blue')
+		ax.add_patch(poly)
+
+plt.show()
