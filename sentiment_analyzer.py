56b1e5f15f4302c43c95584cdd3a9193b1e45c72
smael123 <ismael.alma@gmail.com>
2017-05-07 01:41:49 -0500

    changed get_max_sentiment(), changed the required location of the shape files

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -10,6 +10,8 @@ class Tweet:
 	def __init__(self, text, mood = -1.0):
 		self.text = text
 		self.mood = mood
+		
+		self.get_sentiment()
 	
 	def get_sentiment(self):
 		self.mood = TextBlob(self.text).polarity
@@ -22,7 +24,7 @@ class TwitterQuery:
 		self.str_query = str_query
 		self.region_id = region_id
 		self.region_str = region_str
-		self.tweet_list = []
+		#self.tweet_list = []
 		
 		self.positive_tweets = []
 		self.neutral_tweets = []
@@ -33,17 +35,22 @@ class TwitterQuery:
 	def get_tweets(self):
 		dirty_tweets = self.api.search(q = f"place:{self.region_id} {self.str_query}", count = TwitterQuery.max_tweets)
 		
+		if len(dirty_tweets) == 0:
+			print("No tweets found for that query")
+		
 		clean_text = self.clean_tweets(dirty_tweets)
 		
+		tweet_list = []
+		
 		for tweet in clean_text:
-			self.tweet_list.append(Tweet(tweet))
+			tweet_list.append(Tweet(tweet))
 		
 		#print(tweet.text + " | " + tweet.place.name if tweet.place else "Undefined place")
 		
-		for tweet_obj in self.tweet_list:
-			tweet_obj.get_sentiment()
+		#for tweet_obj in self.tweet_list:
+		#	tweet_obj.get_sentiment()
 		
-		for tweet_obj in self.tweet_list:
+		for tweet_obj in tweet_list:
 			if tweet_obj.mood > 0:
 				self.positive_tweets.append(tweet_obj)
 			elif tweet_obj.mood < 0:
@@ -53,35 +60,34 @@ class TwitterQuery:
 	
 	def get_perc_negative_tweets(self):
 		try:
-			return (len(self.negative_tweets) / len(self.tweet_list)) * 100
+			return (len(self.negative_tweets) / (len(self.negative_tweets) + len(self.neutral_tweets) + len(self.positive_tweets))) * 100
 		except ZeroDivisionError:
 			return 0
 	
 	def get_perc_neutral_tweets(self):
 		try:
-			return (len(self.neutral_tweets) / len(self.tweet_list)) * 100
+			return (len(self.neutral_tweets) / (len(self.negative_tweets) + len(self.neutral_tweets) + len(self.positive_tweets))) * 100
 		except ZeroDivisionError:
 			return 0
 	
 	def get_perc_positive_tweets(self):
 		try:
-			return (len(self.positive_tweets) / len(self.tweet_list)) * 100
+			return (len(self.positive_tweets) / (len(self.negative_tweets) + len(self.neutral_tweets) + len(self.positive_tweets))) * 100
 		except ZeroDivisionError:
 			return 0
 	
 	def get_num_tweets(self):
-		return len(self.tweet_list)
+		return len(self.negative_tweets) + len(self.neutral_tweets) + len(self.positive_tweets)
 	
-	def get_max_sentiment(self):
-		sentiment_dict = {}
-		
-		sentiment_dict[len(self.negative_tweets)] = 'negative'
-		sentiment_dict[len(self.neutral_tweets)] = 'neutral'
-		sentiment_dict[len(self.positive_tweets)] = 'positive'
-		
-		key_list = sentiment_dict.keys()
-		
-		return sentiment_dict[max(key_list)]
+	def get_max_sentiment(self):	
+		if (self.get_perc_positive_tweets() > 50):
+			return 'positive'
+		elif (self.get_perc_neutral_tweets() > 50):
+			return 'neutral'
+		elif (self.get_perc_negative_tweets() > 50):
+			return 'negative'
+		else:
+			return 'none'
 	
 	def get_positive_tweets(self):
 		str = ''
@@ -125,9 +131,9 @@ class TwitterQuery:
 			for link in tweet.entities['urls']:
 				link_list.append(link['url'])
 			
-			mention_list = []
-			for mention in tweet.entities['user_mentions']:
-				mention_list.append(f"@{mention['screen_name']}")
+			#mention_list = []
+			#for mention in tweet.entities['user_mentions']:
+			#	mention_list.append(f"@{mention['screen_name']}")
 				
 			cleaned_tweet = tweet.text
 			
@@ -149,6 +155,7 @@ class TwitterQuery:
 	
 		str = str + (f"Results for {self.region_str}\n")
 		str = str + (f"Positive Percentage: {self.get_perc_positive_tweets()}%\n")
+		str = str + (f"Neutral Percentage: {self.get_perc_neutral_tweets()}%\n")
 		str = str + (f"Negative Percentage: {self.get_perc_negative_tweets()}%\n\n")
 
 		str = str + ("Positive Tweets:\n")
@@ -178,8 +185,8 @@ usa_id = places[0].id
 
 str_query = input("What string would you like to search for? ")
 
-usa_test = TwitterQuery(api, str_query, usa_id, "USA")
-print(usa_test)
+"""usa_test = TwitterQuery(api, str_query, usa_id, "USA")
+print(usa_test)"""
 
 state_list = ("Alabama", "Alaska", "Arizona", "Arkansas", "California","Colorado","Connecticut","Delaware",
 "Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine",
@@ -191,7 +198,7 @@ state_list = ("Alabama", "Alaska", "Arizona", "Arkansas", "California","Colorado
 state_dict = {}
 state_queries = []
 
-for state in state_list:
+for state in state_list[0:5]:
 	places = api.geo_search(query=state, granularity='admin')
 	state_dict[state] = places[0].id
 	query = TwitterQuery(api, str_query, state_dict[state], state)
@@ -203,8 +210,10 @@ for state in state_list:
 map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
         projection='lcc',lat_1=33,lat_2=45,lon_0=-95)
 
+shapefile_loc = os.path.join('necessary_files', 'st99_d00')		  
+		  
 # load the shapefile, use the name 'states'
-map.readshapefile('st99_d00', name='states', drawbounds=True)
+map.readshapefile(shapefile_loc, name='states', drawbounds=True)
 
 # collect the state names from the shapefile attributes so we can
 # look up the shape obect for a state by it's name
@@ -215,22 +224,24 @@ for shape_dict in map.states_info:
 ax = plt.gca() # get current axes instance
 
 # get Texas and draw the filled polygon
-seg = map.states[state_names.index('Texas')]
-poly = Polygon(seg, facecolor='red',edgecolor='red')
-ax.add_patch(poly)
+#seg = map.states[state_names.index('Texas')]
+#poly = Polygon(seg, facecolor='red',edgecolor='red')
+#ax.add_patch(poly)
 
 for state_query in state_queries:
-	if (state_query.get_max_sentiment == 'positive'):
+	if (state_query.get_max_sentiment() == 'positive'):
 		seg = map.states[state_names.index(state_query.region_str)]
 		poly = Polygon(seg, facecolor='red',edgecolor='red')
 		ax.add_patch(poly)
-	elif (state_query.get_max_sentiment == 'neutral'):
+	elif (state_query.get_max_sentiment() == 'neutral'):
 		seg = map.states[state_names.index(state_query.region_str)]
 		poly = Polygon(seg, facecolor='gray',edgecolor='gray')
 		ax.add_patch(poly)
-	else:
+	elif state_query.get_max_sentiment() == 'negative':
 		seg = map.states[state_names.index(state_query.region_str)]
 		poly = Polygon(seg, facecolor='blue',edgecolor='blue')
 		ax.add_patch(poly)
+	else:
+		pass
 
 plt.show()
